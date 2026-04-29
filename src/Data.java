bf6800c12e2f73725dfc6a13fe5b34f74910164d
enzob148_lpsk12 <enzob148@lpsk12.org>
2026-04-29 10:44:58 -0600

    Data

--- src/Data.java
+++ src/Data.java
@@ -2,4 +2,35 @@ import java.util.ArrayList;
 
 public class Data {
     ArrayList<Contact> contacts;
+
+    public Data(){
+        ArrayList<Contact> contacts = new ArrayList<>();
+    }
+
+    public void add(Contact c){
+        contacts.add(c);
+    }
+
+    public void viewAll(){
+        for (int i = 0; i < contacts.size(); i++){
+            System.out.println(contacts.get(i).getName() + " " + contacts.get(i).getNumber());
+        }
+    }
+    public String delete(Contact c){
+        for (int i = 0; i < contacts.size(); i++){
+            if (c.equals(contacts.get(i))){
+                contacts.remove(i);
+                return "Deleted";
+            }
+        }
+        return "Nothing to delete";
+    }
+    public int search(String na){
+        for (int i = 0; i < contacts.size(); i++){
+            if (contacts.get(i).getName().contains(na)){
+                return contacts.get(i).getNumber();
+            }
+        }
+        return -1;
+    }
 }
